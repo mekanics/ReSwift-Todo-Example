196b1de102d6e8ed6289fdca2c371899caa3a304
Ikhsan Assaat <ikhsan.assaat@songkick.com>
2016-11-11 14:28:33 +0100

    add initial ios target

---
+++ ReSwift-Todo-iOS/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ReSwift-Todo-iOS
+//
+//  Created by Ikhsan Assaat on 11/11/2016.
+//  Copyright © 2016 ReSwift. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+
+
+}
+
